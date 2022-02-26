f6dc6c232f3650bc71d3b3dd8a1c892c51f9c95c
= <=>
2022-02-26 15:28:20 -0500

    handleAdd

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,9 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const CounterApp = ({value}) => {
+
+    //HandleAdd
+
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
+
     return (<>
     <h1>CounterApp</h1>
     <h2>{value}</h2>
+
+    <button onClick={handleAdd}>+1</button>
     </>)
 }
 
