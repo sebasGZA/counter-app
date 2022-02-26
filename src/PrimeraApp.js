2cf0ddc2edda979e8de79e694475acca581e31a9
= <=>
2022-02-26 08:59:06 -0500

    Retornar elementos en el Componente - Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,18 @@
+// import React, { Fragment } from 'react';
 import React from 'react';
+
 
 //FC
 
 const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
+    return (
+        // <Fragment>
+        //     <h1>Hola Mundo</h1><p>Mi primera Aplicación</p>
+        // </Fragment>
+        <>
+            <h1>Hola Mundo</h1><p>Mi primera Aplicación</p>
+        </>
+    );
 }
 
 export default PrimeraApp
