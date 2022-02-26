69f8fcd2278ca07a0ffc0a12ca8184d25cafa680
= <=>
2022-02-26 09:05:52 -0500

    Impresión de variables en el HTML

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -5,12 +5,19 @@ import React from 'react';
 //FC
 
 const PrimeraApp = () => {
+    // const saludo = {
+    //     nombre: 'Sebatian',
+    //     edad: 34
+    // };
+    const saludo = 'Hola mundo'
     return (
         // <Fragment>
         //     <h1>Hola Mundo</h1><p>Mi primera Aplicación</p>
         // </Fragment>
         <>
-            <h1>Hola Mundo</h1><p>Mi primera Aplicación</p>
+            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre><p>Mi primera Aplicación</p> */}
+
+            <h1>{saludo}</h1><p>Mi primera Aplicación</p>
         </>
     );
 }
