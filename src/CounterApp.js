3c9048e306c0467a3a45e142a9a458a4208c5f71
= <=>
2022-02-26 15:45:36 -0500

    useState - Hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
-const CounterApp = ({value}) => {
+const CounterApp = ({ value }) => {
+
+    //useStates
+    const [counter, setCounter] = useState(0)
 
     //HandleAdd
 
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        // setCounter(counter+1)
+        setCounter((c) => c + 1)
     }
 
 
     return (<>
-    <h1>CounterApp</h1>
-    <h2>{value}</h2>
+        <h1>CounterApp</h1>
+        <h2>{counter}</h2>
 
-    <button onClick={handleAdd}>+1</button>
+        <button onClick={handleAdd}>+1</button>
     </>)
 }
 
