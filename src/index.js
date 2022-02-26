2c08f303682ab8a726fb603b992d4268ecaabd76
= <=>
2022-02-26 15:12:07 -0500

    Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PrimeraApp from './PrimeraApp'
+// import PrimeraApp from './PrimeraApp'
 import './index.css'
+import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector('#app');
-ReactDOM.render(< PrimeraApp saludo='Hola, soy Gokú' />, divRoot)
+// ReactDOM.render(< PrimeraApp saludo='Hola, soy Gokú' />, divRoot)
+ReactDOM.render(<CounterApp value={10} />, divRoot)
