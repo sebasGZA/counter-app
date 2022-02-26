d7c63171d177a5cf2613af707e28295b04c43a14
= <=>
2022-02-26 15:54:01 -0500

    handleSubtract y handleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
     //useStates
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(value)
 
     //HandleAdd
 
@@ -13,12 +13,21 @@ const CounterApp = ({ value }) => {
         setCounter((c) => c + 1)
     }
 
+    const handleSubtract = () => {
+        setCounter((c) => c > 0 ? c - 1 : 0)
+    }
+
+    const handleReset = () => {
+        setCounter(value)
+    }
 
     return (<>
         <h1>CounterApp</h1>
         <h2>{counter}</h2>
 
         <button onClick={handleAdd}>+1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubtract}>-1</button>
     </>)
 }
 
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector('#app');
 // ReactDOM.render(< PrimeraApp saludo='Hola, soy Gokú' />, divRoot)
-ReactDOM.render(<CounterApp value={10} />, divRoot)
+ReactDOM.render(<CounterApp value={0} />, divRoot)
