b2e8f6a9e0e2e623eb56df740fbfaf2976859834
= <=>
2022-02-27 13:30:36 -0500

    Pruebas en 08-imp-exp.js - Arreglos

---
+++ src/tests/bases/08-imp-exp.test.js
@@ -0,0 +1,34 @@
+import { getHeroeById, getHeroesByOwner } from "../../bases/08-imp-exp"
+import heroes from "../../data/heroes"
+
+describe('Pruebas en funciones de héroes', () => {
+    test('Debe de retornar un héroe', () => {
+        const id = 1
+        const heroe = getHeroeById(id)
+        const heroeData = heroes.find(h => h.id == id)
+        expect(heroe).toEqual(heroeData)
+    })
+
+    test('Debe de retornar undefined si héroe id no existe', () => {
+        const id = 10
+        const heroe = getHeroeById(id)
+        expect(heroe).toBe(undefined)
+    })
+
+    test('Debe de retornar los héroes de DC', () => {
+        const owner = 'DC'
+        const heroes = getHeroesByOwner(owner)
+        const heroesData = heroes.filter(h => h.owner === owner)
+        console.log(heroes.length)
+        expect(heroes).toEqual(heroesData)
+    })
+
+    test('Debe de retornar los héroes de Marvel', () => {
+        const owner = 'Marvel'
+        const heroes = getHeroesByOwner(owner)
+        const heroesData = heroes.filter(h => h.owner === owner)
+        console.log(heroes.length)
+        expect(heroes.length).toBe(heroesData.length)
+    })
+
+})
