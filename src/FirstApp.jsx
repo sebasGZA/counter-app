3592999487a9d6f654abe1b47a754629e9959edf
sebasGZA <sebassga34@gmail.com>
2023-03-18 17:33:54 -0500

    Comunication between components

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,23 +1,11 @@
-// import { Fragment } from "react";
-
-const array = [1, 2, 3, 4, 5, 6];
-// const newMessage = {
-//   message: "Hello world",
-//   title: "Sebastian",
-// };
-
-const getResult = (a, b) => a + b;
-
-export const FirstApp = () => {
+export const FirstApp = ({ title, subTitle }) => {
   return (
-    // <Fragment>
-    //  ......<any/>
-    // </Fragment>
     <>
-      <h1>Hello world</h1>
-      {/* <code>{JSON.stringify(newMessage)}</code> */}
-      <h1>{getResult(15, 10)}</h1>
-      <p>{array}</p>
+      <h1>{title}</h1>
+
+      <p>{subTitle}</p>
     </>
   );
 };
+
+// ReactPropTypes = {};
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp />
+    <FirstApp title={"Sebastian"} subTitle={123} />
   </React.StrictMode>
 );
