65768894d421e0adf16ba976bd4d4f1ce64bd404
sebasGZA <sebassga34@gmail.com>
2023-03-18 17:25:24 -0500

    Add styles.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { HelloWorld } from "./HelloWorld";
 import { FirstApp } from "./FirstApp";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
