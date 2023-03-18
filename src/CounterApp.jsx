4886ebc915fb846458a743e3573efbb47a8ead18
sebasGZA <sebassga34@gmail.com>
2023-03-18 18:19:19 -0500

    handleAdd, handleSubstract and handleReset using useState - Hooks

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,15 +4,25 @@ import PropTypes from "prop-types";
 export const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value);
 
-  const handleAdd = () => setCounter(counter + 1);
   //   const handleAdd = () => setCounter((c) => c + 1);
+  const handleAdd = () => setCounter(counter + 1);
+  const handleSubstract = () => setCounter((c) => (c === 0 ? 0 : c - 1));
+  const handleReset = () => setCounter(value);
 
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
 
-      <button onClick={handleAdd}>+1</button>
+      <button id="substract" onClick={handleSubstract}>
+        -1
+      </button>
+      <button id="reset" onClick={handleReset}>
+        Reset
+      </button>
+      <button id="add" onClick={handleAdd}>
+        +1
+      </button>
     </>
   );
 };
