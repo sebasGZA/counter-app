d169e6fd44cfb8e51e58e720bd6e081c1825088d
sebasGZA <sebassga34@gmail.com>
2023-03-18 20:54:24 -0500

    unit-test toEqual

---
+++ src/test-bases/05-functions.js
@@ -0,0 +1,9 @@
+export const getUser = () => ({
+  uid: "13213",
+  username: "TheKing123",
+});
+
+export const getActiveUser = (name) => ({
+  uid: "ASDSAD5",
+  username: name,
+});
---
+++ src/tests/05-functions.test.js
@@ -0,0 +1,23 @@
+import { getActiveUser, getUser } from "../test-bases/05-functions";
+
+describe("Tests into 05-functions", () => {
+  test("getUser must return an object", () => {
+    const testUser = {
+      uid: "13213",
+      username: "TheKing123",
+    };
+
+    const user = getUser();
+    expect(user).toEqual(testUser);
+  });
+
+  test("getActiveUser must return an object", () => {
+    const name = "Sebastian";
+
+    const user = getActiveUser(name);
+    expect(user).toStrictEqual({
+      uid: "ASDSAD5",
+      username: name,
+    });
+  });
+});
