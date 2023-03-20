e6dbcb9842d98f7fffb4f99b9b3661a11fe5c706
sebasGZA <sebassga34@gmail.com>
2023-03-19 21:26:08 -0500

    gitignore

--- src/tests/FirstApp.test.jsx
+++ src/tests/FirstApp.test.jsx
@@ -1,6 +1,20 @@
-
+import { render } from "@testing-library/react";
+import { FirstApp } from "../FirstApp";
 
 describe("Tests in <FirstApp/>", () => {
+  const title = "Hello, I'm Sebastian";
+
   test("should make match with snapshot", () => {
+    const { container } = render(<FirstApp title={title} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should show title into a h1 tag", () => {
+    const { container, getByText } = render(<FirstApp title={title} />);
+    expect(getByText(title)).toBeTruthy();
+
+    const h1 = container.querySelector("h1");
+    expect(h1.innerHTML).toContain(title);
   });
 });
